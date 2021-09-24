778048727a246fd7ede230a43810dcded316a516
William Gotterer <william.gotterer@gmail.com>
2021-09-24 10:09:19 -0400

    commit tree 6012000dc747c204b0ddc1c9a1e46ea4859e0b8b

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution in this file!
+var customerName="bob"
+
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    var bestCustomer = "not bob";
+
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "Nelly"
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Bob"
+}
+
+
+
+
+
+// var customerName = "bob"
+// function upperCaseCustomerName(){
+//     return customerName.toUpperCase()
+// };
+// function setBestCustomer(){
+//     var bestCustomer = 'not bob'
+//     return bestCustomer
+// };
+// function overwriteBestCustomer(){
+//     var bestCustomer = "bob"
+//     return bestCustomer
+// };
+// const leastFavoriteCustomer = "poor bob"
+// function changeLeastFavoriteCustomer(){
+//     var leastFavoriteCustomer = "rich bob"
+//     return leastFavoriteCustomer
+// }
